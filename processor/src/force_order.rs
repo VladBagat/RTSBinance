10e84c289d2e1c7733ad20be99cd604605add3cb
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2025-12-05 20:57:26 +0000

    nginx

--- processor/src/force_order.rs
+++ processor/src/force_order.rs
@@ -6,34 +6,6 @@ use serde_derive::Serialize;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct ForceOrder {
-    pub e: String,
-    #[serde(rename = "E")]
-    pub e2: i64,
-    pub o: O,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct O {
-    pub s: String,
-    #[serde(rename = "S")]
-    pub s2: String,
-    pub o: String,
-    pub f: String,
-    pub q: String,
-    pub p: String,
-    pub ap: String,
-    #[serde(rename = "X")]
-    pub x: String,
-    pub l: String,
-    pub z: String,
-    #[serde(rename = "T")]
-    pub t: i64,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct ForceOrderFlat {
     pub e: String,
     #[serde(rename = "E")]
